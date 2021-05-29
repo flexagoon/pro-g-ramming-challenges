2792163209487dede3254e90a87103fc4271f9df
flexagoon <flexagoon@pm.me>
2021-05-29 18:31:53 +0300

    improve user-friendliness of 10-todo.zsh

--- practical/10-todo.zsh
+++ practical/10-todo.zsh
@@ -1,13 +1,15 @@
-set -o kshglob
+set -o kshglob # This option is required for line 9 to work
 case $1 in
-	edit)   $EDITOR ~/todo                              ;;
-	list)   cat -n ~/todo                               ;;
-	add)    echo ${@:2} >> ~/todo                       ;;
-	random) grep -v + ~/todo | shuf -n 1                ;;
-	remove)
-		case $2 in
-			(+([0-9])) sed -i $2'd' ~/todo      ;;
-			"")        echo "Missing argument"  ;;
-			*)         sed -i '/'$2'/d' ~/todo  ;;
-		esac                                        ;;
+	edit)   $EDITOR ~/todo                              			       ;;
+	list)   cat -n ~/todo                               			       ;;
+	add)    echo ${@:2} >> ~/todo                       			       ;;
+	# Entries marked with "+" are not included in "random"
+	random) grep -v + ~/todo | shuf -n 1                			       ;;
+	remove)							
+		case $2 in 					
+			(+([0-9])) sed -i $2'd' ~/todo      			       ;;
+			"")        echo "Missing argument"  			       ;;
+			*)         sed -i '/'$2'/d' ~/todo  			       ;;
+		esac                                        			       ;;
+	*)	echo "Wrong action! Possible actions: edit, list, add, random, remove" ;;
 esac
